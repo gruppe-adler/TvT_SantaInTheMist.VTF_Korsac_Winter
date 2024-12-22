9457292a9e17e0f7c84e33c7eff67adc00b950c3
nomisum <nomisum@gmail.com>
2024-12-22 10:53:41 +0100

    add all new files

---
+++ functions/briefcase/cfgFunctions.hpp
@@ -0,0 +1,24 @@
+class mitm_briefcase {
+    class briefcase {
+        class activatePickupPoint {};
+        class addBriefcaseEHs {};
+        class addInteractions {};
+        class attachBriefcase {};
+        class disableCollision {};
+        class dropBriefcase {};
+        class fixBriefcase {};
+        class onAnimChanged {};
+        class onDisconnect {};
+        class onExfilDefended {};
+        class onExfilTriggerToggle {};
+        class onGetIn {};
+        class onGetOut {};
+        class onKilled {};
+        class onUnconscious {};
+        class onWeaponChanged {};
+        class spawnOnWaterSurface {};
+        class showTracker {};
+        class spawnBriefCaseContents {};
+        class trackBriefcase {};
+    };
+};
